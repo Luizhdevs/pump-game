4a2b4a784d26b4be5b3eb1520dd045400ee7f6b0
Luizhdevs <luizhdevs00@gmail.com>
2024-09-27 13:19:32 -0300

    Finalizando a página de regras, apenas o estilo

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Start from './src/pages/Start';
+import Rules from './src/pages/Rules';
+import { StatusBar } from 'react-native';
 
 export default function App() {
-  return <Start />
+  return (
+    <>
+      <StatusBar color="light"/>
+      <Rules/>
+    </>
+  )
 }
 
 
