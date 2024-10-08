894ab9e147f1675910f8aa5961617652b6cfcf6a
Luizhdevs <luizhdevs00@gmail.com>
2024-10-08 11:08:47 -0300

    Finalizando a página de explosão, junto com a implementação da navegação na página de desarmado

--- src/pages/Exploded/index.jsx
+++ src/pages/Exploded/index.jsx
@@ -3,10 +3,10 @@ import { Container, Logo, SucessImg, Title } from "./styled";
 import { useNavigation } from "@react-navigation/native";
 
 import logoImg from "../../assets/logo.png";
-import sucessImg from "../../assets/bomba_cortada_matrix.png";
+import sucessImg from "../../assets/bomba_explodiu.png";
 import ButtonComponents from "../../components/Buttons";
 
-export default function Disarmed() {
+export default function Exploded() {
   const navigation = useNavigation();
 
   function handleNavToStart() {
@@ -16,7 +16,7 @@ export default function Disarmed() {
   return (
     <Container>
       <Logo source={logoImg} style={{ resizeMode: "contain" }} />
-      <Title>Parabéns!!!{"\n"}Você desarmou</Title>
+      <Title>Você falou, a{"\n"}Bomba explodiu!!!</Title>
       <SucessImg source={sucessImg} style={{ resizeMode: "contain" }} />
 
       <ButtonComponents
